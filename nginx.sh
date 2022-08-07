2e8edba524cef53edc512e9732560492a3f0953d
ishwar6 <iisudrj11@gmail.com>
2022-08-07 16:33:05 +0530

    nginx changes

--- nginx.sh
+++ nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/conf.d
+sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 710 /var/lib/jenkins/workspace/django-cicd
-
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
