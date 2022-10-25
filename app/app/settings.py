ea623122d19517dcabf56d5d1c0de75a94c2dc48
ishwar6 <iisudrj11@gmail.com>
2022-10-25 19:05:39 +0530

    drf spectacular

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,10 +21,25 @@ class ModelTests(TestCase):
     def test_new_user_email_normalised(self):
         sample_emails = [
             ['test1@Example.com', 'test1@example.com'],
-            ['Test@exAmple.com', 'Test@example.com'],
-            ['Test@exAmple.COM', 'Test@example.com']
+            ['Test2@exAmple.com', 'Test2@example.com'],
+            ['Test3@exAmple.COM', 'Test3@example.com']
 
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'kasdfj')
             self.assertEqual(user.email, expected)
+    
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
