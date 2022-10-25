46f9f4cca0a898dd3361d385e0925959a8ec1dfb
ishwar6 <iisudrj11@gmail.com>
2022-10-25 19:27:15 +0530

    User serializer

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    'drf_spectacular'
+    'drf_spectacular',
+    'user'
 ]
 
 MIDDLEWARE = [
---
+++ app/user/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+USER = get_user_model()
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user model"""
+    class Meta:
+        model = USER
+        fields = ['email', 'phone', 'name' ]
+        extra_kwargs = {
+            'password': 
+                    {
+                        'write_only': True, 
+                        'min_length': 4
+                    }
+                        }
+    def create(self, validated_data):
+        return USER.objects.create_user(**validated_data)
+
